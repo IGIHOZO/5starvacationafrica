e7196592890771901066ef34c5fdc347ecb90410
IGIHOZO Didier <didierigihozo07@gmail.com>
2024-12-19 12:40:36 +0200

    header image

--- lib/header.php
+++ lib/header.php
@@ -1,45 +1,29 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="utf-8">
-    <title>5*StarVacationAfrica - Travel Agency HTML Template</title>
+    <title>5*StarVacationAfrica - Travel Agency</title>
     <meta content="width=device-width, initial-scale=1.0" name="viewport">
     <meta content="" name="keywords">
     <meta content="" name="description">
-
-    <!-- Google Web Fonts -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link rel="icon" href="../img/images/icon.ico" type="image/x-icon">
     <link href="https://fonts.googleapis.com/css2?family=Heebo:wght@400;500;600&family=Nunito:wght@600;700;800&display=swap" rel="stylesheet">
-
-    <!-- Icon Font Stylesheet -->
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" rel="stylesheet">
-
-    <!-- Libraries Stylesheet -->
     <link href="https://5starvacationafrica.com/lib/animate/animate.min.css" rel="stylesheet">
     <link href="https://5starvacationafrica.com/lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
     <link href="https://5starvacationafrica.com/lib/tempusdominus/css/tempusdominus-bootstrap-4.min.css" rel="stylesheet" />
-
-    <!-- Customized Bootstrap Stylesheet -->
     <link href="https://5starvacationafrica.com/css/bootstrap.min.css" rel="stylesheet">
-
-    <!-- Template Stylesheet -->
     <link href="https://5starvacationafrica.com/css/style.css" rel="stylesheet">
 </head>
-
 <body>
-    <!-- Spinner Start -->
     <div id="spinner" class="show bg-white position-fixed translate-middle w-100 vh-100 top-50 start-50 d-flex align-items-center justify-content-center">
         <div class="spinner-border text-primary" style="width: 3rem; height: 3rem;" role="status">
             <span class="sr-only">Loading...</span>
         </div>
     </div>
-    <!-- Spinner End -->
-
-
-    <!-- Topbar Start -->
     <div class="container-fluid bg-dark px-5 d-none d-lg-block">
         <div class="row gx-0">
             <div class="col-lg-8 text-center text-lg-start mb-2 mb-lg-0">
@@ -60,22 +44,16 @@
             </div>
         </div>
     </div>
-    <!-- Topbar End -->
-
-
-    <!-- Navbar & Hero Start -->
     <div class="container-fluid position-relative p-0">
         <nav class="navbar navbar-expand-lg navbar-light px-4 px-lg-5 py-3 py-lg-0">
             <a href="index.php" class="navbar-brand p-0">
-                <img src="img/images/logo.png" alt="Company Logo" 
-                     style="width: 250px; max-width: 110%; height: 500px!important; margin-top: -20px; 
+                <img src="img/images/logo.png" alt="5 Star Vacation Africa - Logo" style="width: 250px; max-width: 110%; height: 500px!important; margin-top: -20px; 
                             background-color: rgba(255, 255, 255, 0.8); 
                             border-radius: 5px; box-shadow: 0px 4px 8px rgba(0, 0, 0, 0.3);">
             </a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
                 <span class="fa fa-bars"></span>
             </button>
-            
             <div class="collapse navbar-collapse" id="navbarCollapse">
                 <div class="navbar-nav ms-auto py-0">
                     <a href="index.php" class="nav-item nav-link active">Home</a>
