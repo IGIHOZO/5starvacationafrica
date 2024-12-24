83fcd6cc797153741ccc8144b5ed3994ca11ce3c
IGIHOZO Didier <didierigihozo07@gmail.com>
2024-12-24 19:57:37 +0200

    DB

--- lib/drive.php
+++ lib/drive.php
@@ -3,10 +3,16 @@ session_start();
 ini_set('display_errors', '1');
 ini_set('display_startup_errors', '1');
 error_reporting(E_ALL);
+// $host = 'localhost';
+// $dbname = '5startvacationafrica';
+// $username = 'Igihozo';
+// $password = 'Igihozo!#07';
+
 $host = 'localhost';
 $dbname = '5startvacationafrica';
 $username = 'root';
 $password = 'Igihozo!#07';
+
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
